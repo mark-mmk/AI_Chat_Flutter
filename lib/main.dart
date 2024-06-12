1094fe01bf9d5d23dab7636e9b7c211f65f10902
mark <markmekhail1111@gmail.com>
2024-06-13 02:33:17 +0300

    first add

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ai_chat/pages/chat_page.dart';
+import 'package:flutter/material.dart';
+import 'package:ai_chat/consts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ChatPage(),
+    );
+  }
+}
